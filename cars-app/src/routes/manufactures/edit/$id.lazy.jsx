dea28732923fcec584eadd293b2780fa7de68798
chawnima <mferdyalviansyah@gmail.com>
2024-11-14 21:49:08 +0700

    add containers on every route

--- cars-app/src/routes/manufactures/edit/$id.lazy.jsx
+++ cars-app/src/routes/manufactures/edit/$id.lazy.jsx
@@ -6,114 +6,139 @@ import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
 import {
-  getManufacturesById,
-  updateManufacture,
+    getManufacturesById,
+    updateManufacture,
 } from "../../../service/manufacture";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 
 export const Route = createLazyFileRoute("/manufactures/edit/$id")({
-  component: EditManufacture,
+    component: EditManufacture,
 });
 
 function EditManufacture() {
-  const { id } = Route.useParams();
-  const navigate = useNavigate();
-  
-  const { data: manufactureData, isSuccess,isPending } = useQuery({
-    queryKey: ["manufactures", id],
-    queryFn: () => getManufacturesById(id),
-  });
+    const { id } = Route.useParams();
+    const navigate = useNavigate();
 
-  const [name, setName] = useState("");
-  const [logo, setLogo] = useState(undefined);
-  const [currentLogo, setCurrentLogo] = useState(manufactureData?.logo || undefined);
+    const {
+        data: manufactureData,
+        isSuccess,
+        isPending,
+    } = useQuery({
+        queryKey: ["manufactures", id],
+        queryFn: () => getManufacturesById(id),
+    });
 
+    const [name, setName] = useState("");
+    const [logo, setLogo] = useState(undefined);
+    const [currentLogo, setCurrentLogo] = useState(
+        manufactureData?.logo || undefined
+    );
 
-  const { mutate: updateManufactureData,isPending:updatePending } = useMutation({
-    mutationFn: (body) => updateManufacture(id, body),
-    onSuccess: () => {
-      toast.success("Manufacture updated");
-      navigate({ to: "/" });
-    },
-  });
-  useEffect(() => {
-    if (isSuccess) {
-      setName(manufactureData.name);
-      setLogo(manufactureData.logo);
-      setCurrentLogo(manufactureData.logo)
-    }
-  }, [manufactureData, isSuccess]);
-  const onSubmit = async (event) => {
-    event.preventDefault();
+    const { mutate: updateManufactureData, isPending: updatePending } =
+        useMutation({
+            mutationFn: (body) => updateManufacture(id, body),
+            onSuccess: () => {
+                toast.success("Manufacture updated");
+                navigate({ to: "/" });
+            },
+        });
+    useEffect(() => {
+        if (isSuccess) {
+            setName(manufactureData.name);
+            setLogo(manufactureData.logo);
+            setCurrentLogo(manufactureData.logo);
+        }
+    }, [manufactureData, isSuccess]);
+    const onSubmit = async (event) => {
+        event.preventDefault();
 
-    const request = {
-      name,
-      logo,
+        const request = {
+            name,
+            logo,
+        };
+
+        updateManufactureData(request);
     };
 
-    updateManufactureData(request);
-  };
+    if (isPending) {
+        return (
+            <Row className="mt-5">
+                <Col>
+                    <h1 className="text-center">Loading...</h1>
+                </Col>
+            </Row>
+        );
+    }
 
-  if (isPending) {
     return (
-      <Row className="mt-5">
-        <Col>
-          <h1 className="text-center">Loading...</h1>
-        </Col>
-      </Row>
+        <Container>
+            <Row className="justify-content-center">
+                <Col md={6}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Manufacture</Card.Title>
+                            <Form onSubmit={onSubmit}>
+                                <Form.Group className="mb-3" controlId="name">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        value={name}
+                                        onChange={(event) =>
+                                            setName(event.target.value)
+                                        }
+                                    />
+                                </Form.Group>
+                                <Form.Group
+                                    as={Row}
+                                    className="mb-3"
+                                    controlId="logo"
+                                >
+                                    <Form.Label column sm={3}>
+                                        Logo
+                                    </Form.Label>
+                                    <Col sm={9}>
+                                        <Form.Control
+                                            type="file"
+                                            placeholder="Choose File"
+                                            onChange={(event) => {
+                                                setLogo(event.target.files[0]);
+                                                setCurrentLogo(
+                                                    URL.createObjectURL(
+                                                        event.target.files[0]
+                                                    )
+                                                );
+                                            }}
+                                            accept=".jpg,.png"
+                                        />
+                                    </Col>
+                                </Form.Group>
+                                <Form.Group
+                                    as={Row}
+                                    className="mb-3"
+                                    controlId="currentLogo"
+                                >
+                                    <Form.Label column sm={3}></Form.Label>
+                                    <Col sm={9}>
+                                        <Image src={currentLogo} fluid />
+                                    </Col>
+                                </Form.Group>
+                                <Button
+                                    variant="primary"
+                                    type="submit"
+                                    disabled={updatePending}
+                                >
+                                    Save Changes
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
-  }
-
-  return (
-    <Row className="justify-content-center">
-      <Col md={6}>
-        <Card>
-          <Card.Body>
-            <Card.Title>Edit Manufacture</Card.Title>
-            <Form onSubmit={onSubmit}>
-              <Form.Group className="mb-3" controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={name}
-                  onChange={(event) => setName(event.target.value)}
-                />
-              </Form.Group>
-              <Form.Group as={Row} className="mb-3" controlId="logo">
-                <Form.Label column sm={3}>
-                  Logo
-                </Form.Label>
-                <Col sm={9}>
-                  <Form.Control
-                    type="file"
-                    placeholder="Choose File"
-                    onChange={(event) => {
-                      setLogo(event.target.files[0]);
-                      setCurrentLogo(
-                        URL.createObjectURL(event.target.files[0])
-                      );
-                    }}
-                    accept=".jpg,.png"
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row} className="mb-3" controlId="currentLogo">
-                <Form.Label column sm={3}></Form.Label>
-                <Col sm={9}>
-                  <Image src={currentLogo} fluid />
-                </Col>
-              </Form.Group>
-              <Button variant="primary" type="submit" disabled={updatePending}>
-                Save Changes
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  );
 }
 
 export default EditManufacture;
--- cars-app/src/routes/models/$id.lazy.jsx
+++ cars-app/src/routes/models/$id.lazy.jsx
@@ -3,6 +3,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 import { deleteModel, getDetailModel } from "../../service/model";
 import { toast } from "react-toastify";
 import { confirmAlert } from "react-confirm-alert";
@@ -10,111 +11,123 @@ import { useSelector } from "react-redux";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const Route = createLazyFileRoute("/models/$id")({
-  component: ModelDetail,
+    component: ModelDetail,
 });
 
 function ModelDetail() {
-  const { id } = Route.useParams();
-  const navigate = useNavigate();
-  const { user } = useSelector((state) => state.auth);
+    const { id } = Route.useParams();
+    const navigate = useNavigate();
+    const { user } = useSelector((state) => state.auth);
 
-  const {
-    data: model,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ["model", id],
-    queryFn: () => getDetailModel(id),
-    enabled: !!id,
-    retry: false,
-  });
-
-  const { mutate: deleteModelData } = useMutation({
-    mutationFn: () => deleteModel(id),
-    onSuccess: () => {
-      toast.success("Model deleted");
-      navigate({ to: "/" });
-    },
-    onError:()=>{
-      toast.error("Unable to delete")
-    }
-  });
-
-  if (isLoading) {
-    return (
-      <Row className="mt-5">
-        <Col>
-          <h1 className="text-center">Loading...</h1>
-        </Col>
-      </Row>
-    );
-  }
-
-  if (isError || !model) {
-    return (
-      <Row className="mt-5">
-        <Col>
-          <h1 className="text-center">Model is not found!</h1>
-        </Col>
-      </Row>
-    );
-  }
-
-  const onDelete = async (event) => {
-    event.preventDefault();
-
-    confirmAlert({
-      title: "Confirm to delete",
-      message: "Are you sure to delete this data?",
-      buttons: [
-        {
-          label: "Yes",
-          onClick: ()=>{deleteModelData()},
-        },
-        {
-          label: "No",
-          onClick: () => {},
-        },
-      ],
+    const {
+        data: model,
+        isLoading,
+        isError,
+    } = useQuery({
+        queryKey: ["model", id],
+        queryFn: () => getDetailModel(id),
+        enabled: !!id,
+        retry: false,
     });
-  };
 
-  return (
-    <Row className="mt-5">
-      <Col className="offset-md-3">
-        <Card>
-          <Card.Body>
-            <Card.Title>Name: {model?.name}</Card.Title>
-            <Card.Text>Description: {model?.description}</Card.Text>
-            <Card.Text>Manufacture: {model?.manufactures?.name}</Card.Text>
+    const { mutate: deleteModelData } = useMutation({
+        mutationFn: () => deleteModel(id),
+        onSuccess: () => {
+            toast.success("Model deleted");
+            navigate({ to: "/" });
+        },
+        onError: () => {
+            toast.error("Unable to delete");
+        },
+    });
 
-            {user?.role_id === 1 && (
-              <>
-                <Card.Text>
-                  <div className="d-grid gap-2">
-                    <Button
-                      as={Link}
-                      href={`/models/edit/${id}`}
-                      variant="primary"
-                      size="md"
-                    >
-                      Edit Model
-                    </Button>
-                  </div>
-                </Card.Text>
-                <Card.Text>
-                  <div className="d-grid gap-2">
-                    <Button onClick={onDelete} variant="danger" size="md">
-                      Delete Model
-                    </Button>
-                  </div>
-                </Card.Text>
-              </>
-            )}
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col md={3}></Col>
-    </Row>
-  );
+    if (isLoading) {
+        return (
+            <Row className="mt-5">
+                <Col>
+                    <h1 className="text-center">Loading...</h1>
+                </Col>
+            </Row>
+        );
+    }
+
+    if (isError || !model) {
+        return (
+            <Row className="mt-5">
+                <Col>
+                    <h1 className="text-center">Model is not found!</h1>
+                </Col>
+            </Row>
+        );
+    }
+
+    const onDelete = async (event) => {
+        event.preventDefault();
+
+        confirmAlert({
+            title: "Confirm to delete",
+            message: "Are you sure to delete this data?",
+            buttons: [
+                {
+                    label: "Yes",
+                    onClick: () => {
+                        deleteModelData();
+                    },
+                },
+                {
+                    label: "No",
+                    onClick: () => {},
+                },
+            ],
+        });
+    };
+
+    return (
+        <Container>
+            <Row className="mt-5">
+                <Col className="offset-md-3">
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Name: {model?.name}</Card.Title>
+                            <Card.Text>
+                                Description: {model?.description}
+                            </Card.Text>
+                            <Card.Text>
+                                Manufacture: {model?.manufactures?.name}
+                            </Card.Text>
+
+                            {user?.role_id === 1 && (
+                                <>
+                                    <Card.Text>
+                                        <div className="d-grid gap-2">
+                                            <Button
+                                                as={Link}
+                                                href={`/models/edit/${id}`}
+                                                variant="primary"
+                                                size="md"
+                                            >
+                                                Edit Model
+                                            </Button>
+                                        </div>
+                                    </Card.Text>
+                                    <Card.Text>
+                                        <div className="d-grid gap-2">
+                                            <Button
+                                                onClick={onDelete}
+                                                variant="danger"
+                                                size="md"
+                                            >
+                                                Delete Model
+                                            </Button>
+                                        </div>
+                                    </Card.Text>
+                                </>
+                            )}
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={3}></Col>
+            </Row>
+        </Container>
+    );
 }
