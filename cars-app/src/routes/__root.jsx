f9ba44782bcb4f93e1ff515873d2823a9d01d594
“RyukenZzZ” <zxryuken@gmail.com>
2024-11-18 15:30:57 +0700

    Add user page and car filter page

--- cars-app/src/routes/__root.jsx
+++ cars-app/src/routes/__root.jsx
@@ -3,22 +3,30 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-confirm-alert/src/react-confirm-alert.css";
+import UserNavbar from "../components/Navbar/userNavbar";
 import NavigationBar from "../components/Navbar";
+import { useSelector } from "react-redux";
 
 export const Route = createRootRoute({
-    component: () => (
-        <>
-            {/* Navbar */}
-            <NavigationBar />
-
-                {/* Outlet is to detect the pathname or url and then render the component by pathname or url */}
-                <Outlet />
-            
-            {/* This is for debugging router */}
-            <TanStackRouterDevtools />
-
-            {/* React Toastify */}
-            <ToastContainer theme="colored" />
-        </>
-    ),
+  component: RootComponent,
 });
+
+function RootComponent() {
+  const { user } = useSelector((state) => state.auth);
+
+  return (
+    <>
+      {/* Conditional Navbar */}
+      {user?.role_id === 1 ? <NavigationBar /> : <UserNavbar />}
+
+      {/* Outlet for rendering child routes */}
+      <Outlet />
+
+      {/* Debugging Router */}
+      <TanStackRouterDevtools />
+
+      {/* React Toastify */}
+      <ToastContainer theme="colored" />
+    </>
+  );
+}
